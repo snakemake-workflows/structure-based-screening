afcadc5e4b4def4bac294f6bdfb0390b5ac95b86
meesters <meesters@uni-mainz.de>
2023-01-05 11:53:16 +0100

    scripts/mergeOutput.py - added two todo items

--- workflow/scripts/mergeOutput.py
+++ workflow/scripts/mergeOutput.py
@@ -5,11 +5,13 @@ import subprocess
 outFile = snakemake.output[0]
 inFiles = snakemake.input
 
+#TODO: change to ZIPFILE to support better result portability
 with open(outFile, 'wb') as outF:
     for file in inFiles:
         try:
             out = subprocess.check_output(['gunzip', '-t', file])  #test if gzip file is valid before merging
 
+            #TODO: avoid overwriting the file descriptor!
             with open(file, 'rb') as sourceFile:
                 shutil.copyfileobj(sourceFile, outF)
         except subprocess.CalledProcessError:
